8f6f01e35415b7aff6de080a9a28ef1f9f3507f9
David Trautmann <davidtrautmann@Davids-MBP.fritz.box>
2014-09-20 07:30:53 +0200

    Initial Commit

---
+++ BrewT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BrewT
+//
+//  Created by David Trautmann on 20.09.14.
+//  Copyright (c) 2014 David Trautmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
