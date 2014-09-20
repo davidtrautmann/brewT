af68940c190d2ea89cfa85007752898cdc1d1923
David Trautmann <davidtrautmann@Davids-MBP.fritz.box>
2014-09-20 09:27:58 +0200

    Created view and first IBAction functions.

--- BrewT/ViewController.swift
+++ BrewT/ViewController.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var volumeSlider : UISlider!
+    @IBOutlet var volumeLabel : UILabel!
+    @IBOutlet var coldSlider : UISlider!
+    @IBOutlet var coldLabel : UILabel!
+    @IBOutlet var hotSlider : UISlider!
+    @IBOutlet var hotLabel : UILabel!
+    @IBOutlet var targetSlider : UISlider!
+    @IBOutlet var targetLabel : UILabel!
+    @IBOutlet var resultsTextView : UITextView!
+    
+    let calculator = TemperatureModel()
+    let volumeUnit  = "ml"
+    let tempUnit = "° C"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +33,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func volumeSliderChanged() {
+        var value = String(format:"%.0f", volumeSlider.value)
+        volumeLabel.text = "\(value) \(volumeUnit)"
+    }
+    
+    @IBAction func coldSliderChanged() {
+        var value = String(format:"%.0f", coldSlider.value)
+        coldLabel.text = "\(value) \(tempUnit)"
+    }
+    
+    @IBAction func hotSliderChanged() {
+        var value = String(format:"%.0f", hotSlider.value)
+        hotLabel.text = "\(value) \(tempUnit)"
+    }
+    
+    @IBAction func targetSliderChanged() {
+        var value = String(format:"%.0f", targetSlider.value)
+        targetLabel.text = "\(value) \(tempUnit)"
+    }
 }
 
---
+++ TemperatureModel.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureModel.swift
+//  BrewT
+//
+//  Created by David Trautmann on 20.09.14.
+//  Copyright (c) 2014 David Trautmann. All rights reserved.
+//
+
+import Foundation
+
+
+class TemperatureModel {
+    var tempHot: Double = 97.0
+    var tempCold: Double = 16.0
+    var tempTarget: Double = 70.0
+    var volume: Double = 400.0
+}
